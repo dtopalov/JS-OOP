9124c379463b206a5f674c172160bfde29022fc0
dtopalov <dimiter.topalov79@gmail.com>
2015-06-18 17:11:43 +0300

    minor changes

--- 03. Classical Inheritance HW/tasks/task-1.js
+++ 03. Classical Inheritance HW/tasks/task-1.js
@@ -1,19 +1,19 @@
 /* Task Description */
 /* 
-	Create a function constructor for Person. Each Person must have:
-	*	properties `firstname`, `lastname` and `age`
-		*	firstname and lastname must always be strings between 3 and 20 characters, containing only Latin letters
-		*	age must always be a number in the range 0 150
-			*	the setter of age can receive a convertible-to-number value
-		*	if any of the above is not met, throw Error 		
-	*	property `fullname`
-		*	the getter returns a string in the format 'FIRST_NAME LAST_NAME'
-		*	the setter receives a string is the format 'FIRST_NAME LAST_NAME'
-			*	it must parse it and set `firstname` and `lastname`
-	*	method `introduce()` that returns a string in the format 'Hello! My name is FULL_NAME and I am AGE-years-old'
-	*	all methods and properties must be attached to the prototype of the Person
-	*	all methods and property setters must return this, if they are not supposed to return other value
-		*	enables method-chaining
+    Create a function constructor for Person. Each Person must have:
+    *   properties `firstname`, `lastname` and `age`
+        *   firstname and lastname must always be strings between 3 and 20 characters, containing only Latin letters
+        *   age must always be a number in the range 0 150
+            *   the setter of age can receive a convertible-to-number value
+        *   if any of the above is not met, throw Error         
+    *   property `fullname`
+        *   the getter returns a string in the format 'FIRST_NAME LAST_NAME'
+        *   the setter receives a string is the format 'FIRST_NAME LAST_NAME'
+            *   it must parse it and set `firstname` and `lastname`
+    *   method `introduce()` that returns a string in the format 'Hello! My name is FULL_NAME and I am AGE-years-old'
+    *   all methods and properties must be attached to the prototype of the Person
+    *   all methods and property setters must return this, if they are not supposed to return other value
+        *   enables method-chaining
 */
 function solve() {
     var Person = (function() {
@@ -61,8 +61,7 @@ function solve() {
                 this.lastname = value.split(' ')[1];
             },
             enumerable: true,
-            configurable: true,
-            writeable: true
+            configurable: true
         });
 
         Person.prototype.introduce = function() {
@@ -96,4 +95,3 @@ module.exports = solve;
 // var test = new Person('Ivan', 'Ivanov', 25);
 // test.fullname = 'Pesho Gosho';
 // console.log(test.lastname);
-
