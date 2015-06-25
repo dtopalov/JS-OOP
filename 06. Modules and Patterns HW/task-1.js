7316cc3513255bfe6ea71ea19c81dc2a4f0852e9
dtopalov <dimiter.topalov79@gmail.com>
2015-06-26 00:05:43 +0300

    getAllStudents implemented

--- 06. Modules and Patterns HW/task-1.js
+++ 06. Modules and Patterns HW/task-1.js
@@ -1,72 +1,75 @@
-/* Task Description */
-/* 
- * Create a module for a Telerik Academy course
- * The course has a title and presentations
- * Each presentation also has a title
- * There is a homework for each presentation
- * There is a set of students listed for the course
- * Each student has firstname, lastname and an ID
- * IDs must be unique integer numbers which are at least 1
- * Each student can submit a homework for each presentation in the course
- * Create method init
- * Accepts a string - course title
- * Accepts an array of strings - presentation titles
- * Throws if there is an invalid title
- * Titles do not start or end with spaces
- * Titles do not have consecutive spaces
- * Titles have at least one character
- * Throws if there are no presentations
- * Create method addStudent which lists a student for the course
- * Accepts a string in the format 'Firstname Lastname'
- * Throws if any of the names are not valid
- * Names start with an upper case letter
- * All other symbols in the name (if any) are lowercase letters
- * Generates a unique student ID and returns it
- * Create method getAllStudents that returns an array of students in the format:
- * {firstname: 'string', lastname: 'string', id: StudentID}
- * Create method submitHomework
- * Accepts studentID and homeworkID
- * homeworkID 1 is for the first presentation
- * homeworkID 2 is for the second one
- * ...
- * Throws if any of the IDs are invalid
- * Create method pushExamResults
- * Accepts an array of items in the format {StudentID: ..., Score: ...}
- * StudentIDs which are not listed get 0 points
- * Throw if there is an invalid StudentID
- * Throw if same StudentID is given more than once ( he tried to cheat (: )
- * Throw if Score is not a number
- * Create method getTopStudents which returns an array of the top 10 performing students
- * Array must be sorted from best to worst
- * If there are less than 10, return them all
- * The final score that is used to calculate the top performing students is done as follows:
- * 75% of the exam result
- * 25% the submitted homework (count of submitted homeworks / count of all homeworks) for the course
- */
+// /* Task Description */
+// /* 
+//  * Create a module for a Telerik Academy course
+//  * The course has a title and presentations
+//  * Each presentation also has a title
+//  * There is a homework for each presentation
+//  * There is a set of students listed for the course
+//  * Each student has firstname, lastname and an ID
+//  * IDs must be unique integer numbers which are at least 1
+//  * Each student can submit a homework for each presentation in the course
+//  * Create method init
+//  * Accepts a string - course title
+//  * Accepts an array of strings - presentation titles
+//  * Throws if there is an invalid title
+//  * Titles do not start or end with spaces
+//  * Titles do not have consecutive spaces
+//  * Titles have at least one character
+//  * Throws if there are no presentations
+//  * Create method addStudent which lists a student for the course
+//  * Accepts a string in the format 'Firstname Lastname'
+//  * Throws if any of the names are not valid
+//  * Names start with an upper case letter
+//  * All other symbols in the name (if any) are lowercase letters
+//  * Generates a unique student ID and returns it
+//  * Create method getAllStudents that returns an array of students in the format:
+//  * {firstname: 'string', lastname: 'string', id: StudentID}
+//  * Create method submitHomework
+//  * Accepts studentID and homeworkID
+//  * homeworkID 1 is for the first presentation
+//  * homeworkID 2 is for the second one
+//  * ...
+//  * Throws if any of the IDs are invalid
+//  * Create method pushExamResults
+//  * Accepts an array of items in the format {StudentID: ..., Score: ...}
+//  * StudentIDs which are not listed get 0 points
+//  * Throw if there is an invalid StudentID
+//  * Throw if same StudentID is given more than once ( he tried to cheat (: )
+//  * Throw if Score is not a number
+//  * Create method getTopStudents which returns an array of the top 10 performing students
+//  * Array must be sorted from best to worst
+//  * If there are less than 10, return them all
+//  * The final score that is used to calculate the top performing students is done as follows:
+//  * 75% of the exam result
+//  * 25% the submitted homework (count of submitted homeworks / count of all homeworks) for the course
+//  */
 
 function solve() {
     var Course = {
             init: function(title, presentations) {
                 this.title = title;
                 this.presentations = presentations;
+                presentationsCount = presentations.length;
+                this.students = [];
+                studentIDs = 0;
                 return this;
             },
             addStudent: function(name) {
+                var names = name.split(' ');
+                studentIDs += 1;
                 if (!isNameValid(name)) {
                     throw 'Invalid name';
                 }
-                this.names = name.split(' ');
-                this.id = ++studentIDs;
-
-                students.push({
-                    firstname: this.names[0],
-                    lastname: this.names[1],
-                    id: this.id
+                this.students.push({
+                    firstname: names[0],
+                    lastname: names[1],
+                    id: studentIDs
                 });
-                return this.id;
+
+                return studentIDs;
             },
             getAllStudents: function() {
-                return students.slice();
+                return this.students.slice();
             },
             submitHomework: function(studentID, homeworkID) {
                 if (!isStudentIdValid(studentID) || !isHomeworkIdValid(homeworkID)) {
@@ -77,7 +80,8 @@ function solve() {
             getTopStudents: function() {}
         },
         studentIDs = 0,
-        students = [];
+        presentationsCount = 0;
+
 
     Object.defineProperty(Course, 'title', {
         get: function() {
@@ -150,7 +154,7 @@ function solve() {
     }
 
     function isHomeworkIdValid(homeworkID) {
-        if (isNaN(homeworkID) || homeworkID < 1 || homeworkID > Course.presentations.length || (homeworkID !== (homeworkID | 0))) {
+        if (isNaN(homeworkID) || homeworkID < 1 || homeworkID > presentationsCount || (homeworkID !== (homeworkID | 0))) {
             return false;
         }
         return true;
@@ -159,11 +163,12 @@ function solve() {
     return Course;
 }
 
+
 var test = solve();
 var testCourse = test.init('Course title', ['p1', 'p2', 'p3']);
+testCourse.addStudent('Ivan Ivanov');
+console.log(testCourse.students);
+console.log(testCourse.getAllStudents());
 
-console.log(testCourse.title);
-test.addStudent('Ivan Ivanov');
-console.log(test.getAllStudents());
 
 module.exports = solve;
