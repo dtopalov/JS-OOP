1ff33b8d8f7bc988a4ebb88b9b7e889d8dcc6946
dtopalov <dimiter.topalov79@gmail.com>
2015-07-01 12:21:04 +0300

    Functionality for the new tests added

--- 06. Modules and Patterns HW/task-1.js
+++ 06. Modules and Patterns HW/task-1.js
@@ -1,48 +1,48 @@
-// /* Task Description */
-// /* 
-//  * Create a module for a Telerik Academy course
-//  * The course has a title and presentations
-//  * Each presentation also has a title
-//  * There is a homework for each presentation
-//  * There is a set of students listed for the course
-//  * Each student has firstname, lastname and an ID
-//  * IDs must be unique integer numbers which are at least 1
-//  * Each student can submit a homework for each presentation in the course
-//  * Create method init
-//  * Accepts a string - course title
-//  * Accepts an array of strings - presentation titles
-//  * Throws if there is an invalid title
-//  * Titles do not start or end with spaces
-//  * Titles do not have consecutive spaces
-//  * Titles have at least one character
-//  * Throws if there are no presentations
-//  * Create method addStudent which lists a student for the course
-//  * Accepts a string in the format 'Firstname Lastname'
-//  * Throws if any of the names are not valid
-//  * Names start with an upper case letter
-//  * All other symbols in the name (if any) are lowercase letters
-//  * Generates a unique student ID and returns it
-//  * Create method getAllStudents that returns an array of students in the format:
-//  * {firstname: 'string', lastname: 'string', id: StudentID}
-//  * Create method submitHomework
-//  * Accepts studentID and homeworkID
-//  * homeworkID 1 is for the first presentation
-//  * homeworkID 2 is for the second one
-//  * ...
-//  * Throws if any of the IDs are invalid
-//  * Create method pushExamResults
-//  * Accepts an array of items in the format {StudentID: ..., Score: ...}
-//  * StudentIDs which are not listed get 0 points
-//  * Throw if there is an invalid StudentID
-//  * Throw if same StudentID is given more than once ( he tried to cheat (: )
-//  * Throw if Score is not a number
-//  * Create method getTopStudents which returns an array of the top 10 performing students
-//  * Array must be sorted from best to worst
-//  * If there are less than 10, return them all
-//  * The final score that is used to calculate the top performing students is done as follows:
-//  * 75% of the exam result
-//  * 25% the submitted homework (count of submitted homeworks / count of all homeworks) for the course
-//  */
+/* Task Description */
+/* 
+ * Create a module for a Telerik Academy course
+ * The course has a title and presentations
+ * Each presentation also has a title
+ * There is a homework for each presentation
+ * There is a set of students listed for the course
+ * Each student has firstname, lastname and an ID
+ * IDs must be unique integer numbers which are at least 1
+ * Each student can submit a homework for each presentation in the course
+ * Create method init
+ * Accepts a string - course title
+ * Accepts an array of strings - presentation titles
+ * Throws if there is an invalid title
+ * Titles do not start or end with spaces
+ * Titles do not have consecutive spaces
+ * Titles have at least one character
+ * Throws if there are no presentations
+ * Create method addStudent which lists a student for the course
+ * Accepts a string in the format 'Firstname Lastname'
+ * Throws if any of the names are not valid
+ * Names start with an upper case letter
+ * All other symbols in the name (if any) are lowercase letters
+ * Generates a unique student ID and returns it
+ * Create method getAllStudents that returns an array of students in the format:
+ * {firstname: 'string', lastname: 'string', id: StudentID}
+ * Create method submitHomework
+ * Accepts studentID and homeworkID
+ * homeworkID 1 is for the first presentation
+ * homeworkID 2 is for the second one
+ * ...
+ * Throws if any of the IDs are invalid
+ * Create method pushExamResults
+ * Accepts an array of items in the format {StudentID: ..., Score: ...}
+ * StudentIDs which are not listed get 0 points
+ * Throw if there is an invalid StudentID
+ * Throw if same StudentID is given more than once ( he tried to cheat (: )
+ * Throw if Score is not a number
+ * Create method getTopStudents which returns an array of the top 10 performing students
+ * Array must be sorted from best to worst
+ * If there are less than 10, return them all
+ * The final score that is used to calculate the top performing students is done as follows:
+ * 75% of the exam result
+ * 25% the submitted homework (count of submitted homeworks / count of all homeworks) for the course
+ */
 
 function solve() {
     var Course = {
@@ -76,11 +76,16 @@ function solve() {
                     throw 'Invalid ID';
                 }
             },
-            pushExamResults: function(results) {},
+            pushExamResults: function(results) {
+                if (!isExamResultsValid(results)) {
+                    throw 'Invalid results';
+                }
+            },
             getTopStudents: function() {}
         },
         studentIDs = 0,
-        presentationsCount = 0;
+        presentationsCount = 0,
+        examResults = [];
 
 
     Object.defineProperty(Course, 'title', {
@@ -106,6 +111,30 @@ function solve() {
             Course._presentations = value;
         }
     });
+
+    function isExamResultsValid(results) {
+        var i,
+            j,
+            len;
+        if (!(results instanceof Array)) {
+            return false;
+        }
+        if (results.some(function(item) {
+                return (isNaN(item[0]) || isNaN(item[1]) || item[0] < 1 || item[0] > studentIDs);
+            })) {
+            return false;
+        }
+
+        for (i = 0, len = results.length; i < len; i += 1) {
+            for (j = i + 1; j < len; j += 1) {
+                if (results[i][0] === results[j][0]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 
     function isTitleValid(title) {
         if (title.length === 0 || (title !== title.trim()) || title.match(/\s{2,}/)) {
